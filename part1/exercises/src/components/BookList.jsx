85243ed6c3a095bc81945ed1b7da683dec78f83b
Marc Byndas <byndasma@gmail.com>
2024-03-11 17:28:14 -0400

    react part1 exercises

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Norton Anthology of Ancient Music";
+   let book1 = "https://m.media-amazon.com/images/I/81hwRITxd1L._SY522_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/91F6NVlGGjL._SY522_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/61Z4f5F+iFL._SY522_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
+         <img src={book1} alt="The Shape of a Life by Shing-Tung Yau" />
+         <img src={book2} alt="Trust by Hernan Diaz" />
+         <img src={book3} alt="Polymath: The Aquarian Enlightenment by Robert Edward Grant" />
+      </div>
    );
 }
