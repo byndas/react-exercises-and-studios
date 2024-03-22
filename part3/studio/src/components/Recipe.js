9c8a35d734a502fee3f45ddeaccd4102cff972e9
Marc Byndas <byndasma@gmail.com>
2024-03-21 20:49:33 -0400

    finished studio

--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,23 +1,21 @@
-import { useState } from 'react';
-
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://www.blogger.com/profile/11119693459213190159";
+   let authorPhoto = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhziOIfcx0LZYLmFfEC-LNr2e3J8PgfRwGWhSG4gnDjeoKsUXutkiOY3AaprZLQvW5hbC99_Djyw23or8RCC5ei9bpGdGJhH_nryhW82uzcGOcJq9AajC-iqcU4TtQB9Q/s220/DSC00085.jpg";
+   let authorName = "Turkish Cook";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt = "female portrait photo" style={{objectFit: "contain", borderRadius: "50%"}} />
          <div>
             <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
+            <a href={authorLink}></a>
          </div>
       </div>
    );
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["spinach", "onion", "flour", "chicken stock", "egg", "yogurt", "oil"];
    return(
       <div>
          <h3>Recipe Ingredients</h3>
@@ -27,6 +25,8 @@ const RecipeIngredients = () => {
             <li>{ingredients[2]}</li>
             <li>{ingredients[3]}</li>
             <li>{ingredients[4]}</li>
+            <li>{ingredients[5]}</li>
+            <li>{ingredients[6]}</li>
          </ul>
       </div>
    );
@@ -34,10 +34,10 @@ const RecipeIngredients = () => {
 
 const RecipeDescription = () => {
    return (
-      <div> 
+      <div>
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Spinach Soup </h1>
+            <p>A healthy and nutritious recipe from my mom.</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -49,7 +49,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://3.bp.blogspot.com/_tqAuwGaWySU/SaDsEj8KadI/AAAAAAAAFJ4/izxcH2focRk/s1600-h/Spinach+Soup+(Ispanak+Corbasi).JPG" alt="" className="imageUpdates"/>
    );
 }
 
